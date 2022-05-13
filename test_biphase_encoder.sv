437e057293b063f97b0b05868cd662b98dc6a793
Douglas P. Fields, Jr <symbolics@lisp.engineer>
2022-05-12 23:49:41 -0400

    Much stronger, multi-data-send at various points in the biphase cycle test harness

--- test_biphase_encoder.sv
+++ test_biphase_encoder.sv
@@ -28,6 +28,10 @@ logic busy_out;
 logic dbg_first_half;
 logic dbg_current_bit;
 
+// What we're sending
+logic data_ready;
+logic [7:0] data_in;
+
 biphase_encoder #(
   // We don't need to do 75 kHz; we can do much faster for our simulator
   .SHORT_PULSE(10)
@@ -35,10 +39,10 @@ biphase_encoder #(
   .clk(clock),
   .rst(reset),
 
-  // Just keep repeating this data
-  .data_ready('1),
+  // Assert when !busy_out
+  .data_ready,
   // Data is sent LSB first
-  .data_in(8'b1010_0111),
+  .data_in,
 
   // Our encoded data stream output
   .biphase_out,
@@ -52,19 +56,83 @@ biphase_encoder #(
   .dbg_current_bit
 );
 
+///////////////////////////////////////////////////////////////
+// Data driver for the test.
+// We want to test asserting the data_ready signal at all possible
+// points in the biphase encoder cycle. Since it uses a short pulse
+// of 10, it has 20 possible start points, so we will use 2^5 delay count
+// after busy is de-asserted to cover all the possibilities.
+
+// Send after a certain number of cycle delay
+logic [4:0] round_num;
+logic [4:0] start_delay;
+logic       last_busy_out;
+
+// Send a character every time it is not busy,
+// after waiting a different number of cycles each time.
+always_ff @(posedge clock) begin
+
+  last_busy_out <= busy_out;
+
+  if (!reset) begin
+
+    if (!busy_out) begin
+
+      if (last_busy_out) begin
+        // We just got unbusy, cleared to send in a bit
+        $display("Moving to next round @ ", $time, " ", 
+                 start_delay, " ", round_num, " ", last_busy_out);
+        start_delay <= round_num;
+        round_num <= round_num - 5'd1;
+      end else begin
+        // Wait until we send it
+        start_delay <= start_delay - 5'b1;
+      end
+
+      if (start_delay == 0 || 
+          (round_num == 0 && last_busy_out)) begin
+        $display("Starting to send     @ ", $time, " ", 
+                 start_delay, " ", round_num, " ", last_busy_out);
+        // Send the data!
+        data_ready <= '1;
+        data_in[7] <= '1;
+        data_in[6:2] <= round_num;
+        data_in[1:0] <= 2'b01;
+      end
+
+    end else begin
+      // The system is busy right now so...
+      data_ready <= '0;
+    end // !busy_out
+
+  end else begin
+    // Reset
+    data_ready <= '0;
+    round_num <= 5'b1_1111;
+    start_delay <= 5'b1_1111;
+  end
+
+end // Send different bytes after different delays
+
+///////////////////////////////////////////////////////////////
+// Reset driver and end of test handler
 
 // initialize test with a reset for 22 ns
 initial begin
-  $display("Starting Simulation @ ", $time);
+  $display("Starting Simulation  @ ", $time);
   clock <= 1'b0;
   reset <= 1'b1; 
   #22; reset <= 1'b0;
-  // Stop the simulation at appropriate point
-  #5001;
-  $display("Ending simulation @ ", $time);
+
+  // Stop the simulation at appropriate point.
+  // 20 clocks x 10 bits = 200 clocks x 10ns per clock = 2000ns,
+  // plus up to 32 clocks between, run at least 32 times
+  #200_000;
+  $display("Ending simulation    @ ", $time);
   $stop; // $stop = breakpoint
   // DO NOT USE $finish; it will exit Questa!!!
 end
+
 
 endmodule
 
--- wave.do
+++ wave.do
@@ -9,9 +9,15 @@ add wave -noupdate /test_biphase_encoder/dut/first_half_begun
 add wave -noupdate /test_biphase_encoder/dut/second_half_begun
 add wave -noupdate /test_biphase_encoder/dut/next_bit_index
 add wave -noupdate /test_biphase_encoder/dbg_current_bit
+add wave -noupdate -divider Biphase
 add wave -noupdate /test_biphase_encoder/biphase_out
+add wave -noupdate -divider Driver
+add wave -noupdate /test_biphase_encoder/data_ready
+add wave -noupdate /test_biphase_encoder/round_num
+add wave -noupdate /test_biphase_encoder/start_delay
+add wave -noupdate /test_biphase_encoder/data_in
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {1451629 ps} 0}
+WaveRestoreCursors {{Cursor 1} {687417 ps} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 258
 configure wave -valuecolwidth 100
@@ -27,4 +33,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ns
 update
-WaveRestoreZoom {0 ps} {6205873 ps}
+WaveRestoreZoom {0 ps} {262072843 ps}
